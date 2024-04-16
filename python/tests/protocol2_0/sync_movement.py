ee5854e5778a3edaf9351133ba26c0c3c164f4e4
NO3 <983509876@qq.com>
2024-04-16 20:07:32 +0800

    add sync movement feature

--- python/tests/protocol2_0/sync_movement.py
+++ python/tests/protocol2_0/sync_movement.py
@@ -47,13 +47,14 @@ ADDR_PRESENT_POSITION       = 580
 LEN_PRESENT_POSITION        = 4          # Data Byte Length
 BAUDRATE                    = 2000000
 ADDR_DRIVE_MODE             = 10;
+ADDR_OPERATING_MODE         = 11;
 
 # DYNAMIXEL Protocol Version (1.0 / 2.0)
 # https://emanual.robotis.com/docs/en/dxl/protocol2/
 PROTOCOL_VERSION            = 2.0
 
 # Make sure that each DYNAMIXEL ID should have unique ID.
-DXL_ID = [1, 2, 3, 4, 5, 6]
+DXL_ID = [1, 2]
 
 
 # Use the actual port assigned to the U2D2.
@@ -66,9 +67,9 @@ TORQUE_DISABLE              = 0                 # Value for disabling the torque
 DXL_MOVING_STATUS_THRESHOLD = 20                # Dynamixel moving status threshold
 PROFILE_ENABLE              = 0x0;              # Value for enable trajectory profile
 PROFILE_DISABLE             = 0x02;             # Value for disable trajectory profile
+CURRENT_CONTROL_MODE        = 0;                # Value for current control mode
 
-index = 0
-dxl_goal_position = [0, 1, 5, 10, 18, 28, 41, 56, 73, 92, 114, 137, 164, 192, 223, 256, 291, 328, 368, 410, 455, 501, 550, 601, 655, 710, 768, 828, 891, 956, 1023, 1092, 1164, 1237, 1313, 1388, 1464, 1539, 1614, 1690, 1765, 1840, 1916, 1991, 2067, 2142, 2217, 2293, 2368, 2443, 2519, 2594, 2670, 2745, 2820, 2896, 2971, 3046, 3122, 3197, 3273, 3348, 3423, 3499, 3574, 3649, 3725, 3800, 3872, 3943, 4011, 4077, 4141, 4202, 4261, 4318, 4372, 4425, 4475, 4522, 4568, 4611, 4652, 4691, 4727, 4761, 4793, 4822, 4850, 4875, 4897, 4918, 4936, 4952, 4966, 4977, 4986, 4993, 4997, 5000, 5000, 4997, 4993, 4986, 4977, 4966, 4952, 4936, 4918, 4897, 4875, 4850, 4822, 4793, 4761, 4727, 4691, 4652, 4611, 4568, 4522, 4475, 4425, 4372, 4318, 4261, 4202, 4141, 4077, 4011, 3943, 3872, 3800, 3725, 3649, 3574, 3499, 3423, 3348, 3273, 3197, 3122, 3046, 2971, 2896, 2820, 2745, 2670, 2594, 2519, 2443, 2368, 2293, 2217, 2142, 2067, 1991, 1916, 1840, 1765, 1690, 1614, 1539, 1464, 1388, 1313, 1237, 1164, 1092, 1023, 956, 891, 828, 768, 710, 655, 601, 550, 501, 455, 410, 368, 328, 291, 256, 223, 192, 164, 137, 114, 92, 73, 56, 41, 28, 18, 10, 5, 1, 0]         # Goal position
+dxl_goal_position = 0;         # Goal position
 
 # Initialize PortHandler instance
 # Set the port path
@@ -105,6 +106,7 @@ else:
     getch()
     quit()
 
+# Homing
 for i in range(0, len(DXL_ID)):
     # Enable Dynamixel Torque
     dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID[i], ADDR_TORQUE_ENABLE, TORQUE_ENABLE)
@@ -123,7 +125,7 @@ for i in range(0, len(DXL_ID)):
         print("%s" % packetHandler.getRxPacketError(dxl_error))
 
     # Write start position point
-    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID[i], ADDR_GOAL_POSITION, dxl_goal_position[0])
+    dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID[i], ADDR_GOAL_POSITION, dxl_goal_position)
     
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
@@ -140,53 +142,50 @@ for i in range(0, len(DXL_ID)):
         elif dxl_error != 0:
             print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-        print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID[i], dxl_goal_position[0], dxl_present_position))
+        print("[ID:%03d] GoalPos:%03d  PresPos:%03d" % (DXL_ID[i], dxl_goal_position, dxl_present_position))
 
-        if not abs(dxl_goal_position[0] - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
+        if not abs(dxl_goal_position - dxl_present_position) > DXL_MOVING_STATUS_THRESHOLD:
             break    
 
-    # Disable trajectory profile
-    dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID[i], ADDR_DRIVE_MODE, PROFILE_DISABLE)
+# Configure master motor operating mode to current control
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID[0], ADDR_OPERATING_MODE, CURRENT_CONTROL_MODE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))        
+
+# Disable slave motor trajectory profile
+dxl_comm_result, dxl_error = packetHandler.write1ByteTxRx(portHandler, DXL_ID[1], ADDR_DRIVE_MODE, PROFILE_DISABLE)
+if dxl_comm_result != COMM_SUCCESS:
+    print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+elif dxl_error != 0:
+    print("%s" % packetHandler.getRxPacketError(dxl_error))
+
+while 1:
+    # Read present position
+    dxl_present_position, dxl_comm_result, dxl_error = packetHandler.read4ByteTxRx(portHandler, DXL_ID[0], ADDR_PRESENT_POSITION)
+    # dxl_present_position = struct.unpack('i', struct.pack('I', dxl_present_position))[0];
+    # if dxl_present_position == -65536:
+    #     dxl_present_position = 0;
+    
+    if abs(dxl_present_position - dxl_goal_position) < 10000:
+        dxl_goal_position = dxl_present_position;
+    
     if dxl_comm_result != COMM_SUCCESS:
         print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
     elif dxl_error != 0:
-        print("%s" % packetHandler.getRxPacketError(dxl_error))        
-    
-    # Add parameter storage for Dynamixel present position value
-    dxl_addparam_result = groupSyncRead.addParam(DXL_ID[i])
-    if dxl_addparam_result != True:
-        print("[ID:%03d] groupSyncRead addparam failed" % DXL_ID[i])
-        quit()
-
-while 1:
-    print("Press any key to continue! (or press ESC to quit!)")
-    if getch() == chr(0x1b):
-        break
-
-    for index in range(0, len(dxl_goal_position)):
-        # Allocate goal position value into byte array
-        param_goal_position = [DXL_LOBYTE(DXL_LOWORD(dxl_goal_position[index])), DXL_HIBYTE(DXL_LOWORD(dxl_goal_position[index])), DXL_LOBYTE(DXL_HIWORD(dxl_goal_position[index])), DXL_HIBYTE(DXL_HIWORD(dxl_goal_position[index]))]
-
-        for i in range(0, len(DXL_ID)):
-            # Add Dynamixel goal position value to the Syncwrite parameter storage
-            dxl_addparam_result = groupSyncWrite.addParam(DXL_ID[i], param_goal_position)
-            if dxl_addparam_result != True:
-                print("[ID:%03d] groupSyncWrite addparam failed" % DXL_ID[i])
-                quit()
-
-        # Syncwrite goal position
-        dxl_comm_result = groupSyncWrite.txPacket()
+        print("%s" % packetHandler.getRxPacketError(dxl_error))
+    else:
+        # Write goal position
+        dxl_comm_result, dxl_error = packetHandler.write4ByteTxRx(portHandler, DXL_ID[1], ADDR_GOAL_POSITION, dxl_goal_position)
+        
         if dxl_comm_result != COMM_SUCCESS:
             print("%s" % packetHandler.getTxRxResult(dxl_comm_result))
+        elif dxl_error != 0:
+            print("%s" % packetHandler.getRxPacketError(dxl_error))
 
-        # Clear syncwrite parameter storage
-        groupSyncWrite.clearParam()
-
-        # Wait for movement to goal position
-        time.sleep(0.01)
-
-# Clear syncread parameter storage
-groupSyncRead.clearParam()
+    # Wait for movement to goal position
+    time.sleep(0.01)
 
 for i in range(0, len(DXL_ID)):
     # Enable trajectory profile
